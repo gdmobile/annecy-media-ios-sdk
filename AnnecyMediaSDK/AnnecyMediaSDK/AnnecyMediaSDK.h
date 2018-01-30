ac9fe8e68e03dcbd6c552357c96273d4a66a806b
schultka <mail@tobias-schultka.com>
2018-01-30 13:57:44 +0100

    initialized project

---
+++ AnnecyMediaSDK/AnnecyMediaSDK/AnnecyMediaSDK.h
@@ -0,0 +1,19 @@
+//
+//  AnnecyMediaSDK.h
+//  AnnecyMediaSDK
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnnecyMediaSDK.
+FOUNDATION_EXPORT double AnnecyMediaSDKVersionNumber;
+
+//! Project version string for AnnecyMediaSDK.
+FOUNDATION_EXPORT const unsigned char AnnecyMediaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnnecyMediaSDK/PublicHeader.h>
+
+
