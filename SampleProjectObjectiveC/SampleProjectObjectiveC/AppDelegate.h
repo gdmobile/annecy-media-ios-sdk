91271848786cd82164f9bfbec7d80b47cf5c600c
schultka <mail@tobias-schultka.com>
2018-01-30 16:32:38 +0100

    initialized Objective-C sample project

---
+++ SampleProjectObjectiveC/SampleProjectObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleProjectObjectiveC
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleProjectObjectiveC/SampleProjectObjectiveC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SampleProjectObjectiveC
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)annecyOnOfferwallReadyWithViewController:(AMSDKViewController * _Nonnull)viewController {
+    
+}
+
+@end
