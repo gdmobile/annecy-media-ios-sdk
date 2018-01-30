f4f8f6ca3bc5ae6cbb268f72998d82ce2aa473a6
schultka <mail@tobias-schultka.com>
2018-01-30 16:54:35 +0100

    added Objective-C debug project

---
+++ DebugProject/DebugProjectObjC/DebugProjectObjC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DebugProjectObjC
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)annecyOnOfferwallReadyWithViewController:(AMSDKViewController * _Nonnull)viewController {
+    
+}
+
+@end
---
+++ DebugProject/DebugProjectObjC/DebugProjectObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DebugProjectObjC
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
