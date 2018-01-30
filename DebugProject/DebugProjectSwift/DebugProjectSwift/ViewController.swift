2cfa5be37fe563ccea5a3bdfff85758aa78e07d0
schultka <mail@tobias-schultka.com>
2018-01-30 16:51:30 +0100

    added Swift debug project

---
+++ DebugProject/DebugProjectSwift/DebugProjectSwift/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DebugProjectSwift
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+import UIKit
+import AnnecyMediaSDK
+
+class ViewController: UIViewController, AMSDKDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        AnnecyMediaSDK.instance.delegate = self
+        AnnecyMediaSDK.instance.options.token = "6ce0bbf0-2dc8-4d7c-a497-e93105188ba1"
+        AnnecyMediaSDK.instance.options.userId = "foo"
+        AnnecyMediaSDK.instance.requestOfferwall()
+    }
+    
+    func annecyOnOfferwallReady(viewController: AMSDKViewController) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
