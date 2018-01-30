dbf055b853bd80e6c479626d73695d563b940c5b
schultka <mail@tobias-schultka.com>
2018-01-30 20:36:05 +0100

    added CocoaPods Objective-C sample project

---
+++ SampleProject/SampleObjCPods/SampleObjCPods/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SampleObjCPods
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AnnecyMediaSDK/AnnecyMediaSDK-Swift.h>
+
+@interface ViewController : UIViewController<AMSDKDelegate>
+
+@end
---
+++ SampleProject/SampleObjCPods/SampleObjCPods/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  SampleObjCPods
+//
+//  Created by Tobias Schultka on 30.01.18.
+//  Copyright © 2018 GD Mobile GmbH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+AnnecyMediaSDK* annecyMediaSDK;
+NSString* TOKEN = @"6ce0bbf0-2dc8-4d7c-a497-e93105188ba1";
+NSString* USER_ID = @"foo";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    annecyMediaSDK = AnnecyMediaSDK.instance;
+    annecyMediaSDK.delegate = self;
+    [annecyMediaSDK setOptionsWithToken:TOKEN userId:USER_ID];
+    [annecyMediaSDK requestOfferwall];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)annecyOnOfferwallReadyWithViewController:(AMSDKViewController * _Nonnull)viewController {
+    [self presentViewController:viewController animated:YES completion:nil];
+}
+
+@end
